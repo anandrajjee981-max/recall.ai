4eb652b1ef857dcfdf3202674c69e85cd04c366f
anand raj <git add README.md>
2026-07-05 21:14:34 +0530

    add langgraph

--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -1,5 +1,18 @@
 
+import { ChatMistralAI } from "@langchain/mistralai"
+import { ChatCohere } from "@langchain/cohere"
 
+ export const cohereLLM = new ChatCohere({
+    model: "command-r-plus",
+   apiKey: process.env.COHERE_API_KEY,
+    
+})
+
+export const mistralLLM = new ChatMistralAI({
+    model: "mistral-large-latest",
+   apiKey: process.env.MISTRAL_API_KEY,
+   
+})
 
 
 
